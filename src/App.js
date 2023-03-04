34f2f1e25db1c2a464133a2e1e68dca871f0c1f7
Maatouf-Saad <saad_maatouf@hotmail.fr>
2023-03-04 15:04:27 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className='container'>
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
