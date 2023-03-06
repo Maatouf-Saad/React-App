4bd5a624ff920f6021e45bf1becdf6575b232742
Maatouf-Saad <saad_maatouf@hotmail.fr>
2023-03-06 13:56:42 +0100

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        dishes: DISHES
+      };    
+  }
+
+    render() {
+      return (
+        <div>
+          <Navbar dark color="primary">
+            <div className='container'>
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
         </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+      );
+    }
 }
 
 export default App;
