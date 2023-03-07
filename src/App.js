ed404d5289461729f3de12c77fcad0b4381e28e9
Maatouf-Saad <saad_maatouf@hotmail.fr>
2023-03-07 14:13:03 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
     render() {
       return (
+        <BrowserRouter>
         <div>
           <Main />
         </div>
+        </BrowserRouter>
       );
     }
 }
