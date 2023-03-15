08873fb648880bd93c270a0b697efe109ce50523
Maatouf-Saad <saad_maatouf@hotmail.fr>
2023-03-15 10:27:49 +0100

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -25,9 +25,8 @@ class CommentForm extends Component {
    }
    
    handleSubmit(values) {
-      console.log("Current State is: " + JSON.stringify(values));
-      alert("Current State is: " + JSON.stringify(values));
       this.toggleModal();
+      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
    }
    
    render() {
@@ -119,7 +118,7 @@ class CommentForm extends Component {
     }
   }
 
-  function RenderComments({comments}) {
+  function RenderComments({comments, addComment, dishId}) {
     if (comments != null )
       return(
         <div className="col-12 col-md-5 m-1">
@@ -134,7 +133,7 @@ class CommentForm extends Component {
                 );
             })}
           </ul>
-            <CommentForm />
+            <CommentForm dishId ={dishId} addComment={addComment} />
         </div>
       );
 
@@ -161,7 +160,9 @@ class CommentForm extends Component {
           </div>
           <div className="row">
               <RenderDish dish={props.dish} />
-              <RenderComments comments={props.comments} />
+              <RenderComments comments={props.comments}
+                  addComment={props.addComment}
+                  dishId={props.dish.id} />
           </div>
         </div>
       );
